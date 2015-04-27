c1f0a263eef48e863341ac86557cde82c294e6c5
eM <whoknowwhat0623@gmail.com>
2015-04-27 12:50:09 +0900

    setup.py update

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from distutils.core import setup
 
 setup(name='marumaru-downloader',
-      version='0.1.0',
+      version='0.1.1',
       description='Comic Downloader for marumaru.in',
       url='https://bitbucket.org/whoknowwhat/marumaru-downloader',
       author='eM',
       author_email='whoknowwhat0623@gmail.com',
       packages=['marumaru_downloader'],
-      install_requires=['requests==2.3.0',
-                        'beautifulsoup4==4.3.2',
-                        'Pillow==2.7.0'],
+      install_requires=['requests>=2.3.0',
+                        'beautifulsoup4>=4.3.2',
+                        'Pillow>=2.7.0'],
       zip_safe=False)
